0936793b872517e476e5811d5e27ea21878145d7
Santosh Moktan <santoshmoktan@lftechnology.com>
2013-08-03 13:14:24 +0545

    New app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teachanimal::Application.config.secret_key_base = '9af3448ef57e0068e39006d138a862d6799e3463b7bc5615a7901a3622de0a57b431acb18beb01f5c38c0dd08127a2c704f660b7a43b7ac66f3bd87a83b3dbee'
